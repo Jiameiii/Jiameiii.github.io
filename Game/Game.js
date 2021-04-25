ed684bf1ce8aa19937ce21dd6f6481b49ca1963f
Jiameiii <lijiamei0706@gmail.com>
2021-04-24 17:28:16 -0700

    Update Game.js

--- Game/Game.js
+++ Game/Game.js
@@ -10,8 +10,8 @@ var img;
 var img2;
 
 function preload(){
-  img = loadImage('https://jiameiii.github.io/Game/virus.png');
-  img2 = loadImage('https://jiameiii.github.io/Game/virus.png');
+  img = loadImage('virus.png');
+  img2 = loadImage('virus.png');
 }
 
 function setup() {
