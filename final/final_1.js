f606f8484041ee059fab6c63bbff1b41e42a6475
Jiameiii <lijiamei0706@gmail.com>
2021-05-20 23:50:10 -0700

    Final

--- final/final_1.js
+++ final/final_1.js
@@ -1,15 +1,24 @@
 let img;
+let imgg;
 let img1;
+let img11;
 let img2;
+let img22;
 let img3;
+let img33;
 function preload() {
-  img = loadImage('photo/moon.gif');
-  img1 = loadImage('photo/flower.gif');
-  img2 = loadImage('photo/skull.gif');
-  img3 = loadImage('photo/stoicism.gif');
+  img = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/moon.gif');
+imgg = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/moon.gif');
+  img1 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/flower.gif');
+img11 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/flower.gif');
+  img2 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/skull.gif');
+img22 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/skull.gif');
+  
+	img3 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/stoicism.gif');
+img33 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/stoicism.gif');
 }
 function setup() {
-  createCanvas(2000, 2000);  // canvas size
+  createCanvas(3000, 3000);  // canvas size
   background(600);
   // Top-left corner of the img is at (10, 10)
   // Width and height are 50 x 50
@@ -17,7 +26,11 @@ function setup() {
 
 function draw() {
     image(img, 10, 10,620,800);
-  image(img1, 700, 10, 620,800 );
-  image(img2, 10,900);
-  image(img3, 700, 900);
+	image(imgg, 700, 10, 620,800 );
+	image(img1, 1000, 10, 620,800 );
+image(img11, 1000, 10, 620,800 );
+	image(img2, 10,900);
+image(img22, 10,1500);
+	image(img3, 700, 900);
+image(img33, 900, 1100);
 }
