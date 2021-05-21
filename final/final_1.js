d4c2811181522b7a7f7840862af201bbea84fe9f
Jiameiii <lijiamei0706@gmail.com>
2021-05-21 00:20:16 -0700

    Final

--- final/final_1.js
+++ final/final_1.js
@@ -8,17 +8,16 @@ let img3;
 let img33;
 function preload() {
   img = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/moon.gif');
-imgg = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/moon.gif');
+imgg = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/moon.1.gif');
   img1 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/flower.gif');
-img11 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/flower.gif');
+img11 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/flower.1.gif');
   img2 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/skull.gif');
-img22 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/skull.gif');
-  
+img22 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/skull.1.gif');
 	img3 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/stoicism.gif');
-img33 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/stoicism.gif');
+img33 = loadImage('https://github.com/Jiameiii/Jiameiii.github.io/blob/main/final/photo/stoicism.1.gif');
 }
 function setup() {
-  createCanvas(3000, 3000);  // canvas size
+  createCanvas(2800, 1800);  // canvas size
   background(600);
   // Top-left corner of the img is at (10, 10)
   // Width and height are 50 x 50
@@ -26,11 +25,14 @@ function setup() {
 
 function draw() {
     image(img, 10, 10,620,800);
-	image(imgg, 700, 10, 620,800 );
-	image(img1, 1000, 10, 620,800 );
-image(img11, 1000, 10, 620,800 );
-	image(img2, 10,900);
-image(img22, 10,1500);
-	image(img3, 700, 900);
-image(img33, 900, 1100);
+  image(imgg, 700, 10, 620,800 );
+  
+  image(img1, 1400, 10, 620,800 );
+image(img11, 2100, 10, 620,800 );
+
+  image(img2, 10,900);
+image(img22, 700,900,620,800);
+
+  image(img3, 1400, 900);
+image(img33, 2100, 900, 620, 800);
 }
